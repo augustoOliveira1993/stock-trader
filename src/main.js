b81c20452f9836c83aaa35b011d398c83a448eba
augusto <gutim160@gmail.com>
2021-10-25 22:21:51 -0300

    url firebase main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
+// https://stock-trade-c83f0-default-rtdb.firebaseio.com/
+
 Vue.config.productionTip = false
 
 new Vue({
