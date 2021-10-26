e96ebb033f09dc783fd9345e4ebd71437066c0b1
augusto <gutim160@gmail.com>
2021-10-25 23:29:40 -0300

    Stock & Stocks

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
+import router from './router'
 // https://stock-trade-c83f0-default-rtdb.firebaseio.com/
 
 Vue.config.productionTip = false
 
 new Vue({
+	router,
 	render: h => h(App),
 }).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './components/Home'
-import Portfolio from './components/portfolio/Portfolio'
+import Portifolio from './components/portifolio/Portifolio.vue'
 import Stocks from './components/stocks/Stocks'
 
 Vue.use(Router)
@@ -11,7 +11,7 @@ export default new Router({
     mode: 'history',
     routes: [
         { path: '/', component: Home },
-        { path: '/portfolio', component: Portfolio },
+        { path: '/portfolio', component: Portifolio },
         { path: '/stocks', component: Stocks },
     ]
 })
